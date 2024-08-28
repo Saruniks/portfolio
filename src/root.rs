b2102f21050e2d5a5ffe474ae82601b5ac86f316
sarunas.gincas <sarunikss@gmail.com>
2024-08-28 15:20:18 +0300

    Add icons and about me

--- src/root.rs
+++ src/root.rs
@@ -1,12 +1,14 @@
 use crate::root_router_switch::RouterSwitch;
 use crate::routes::Route;
 use fallout_ui::components::layout::content::Content;
+use fallout_ui::components::layout::footer::Footer;
 use fallout_ui::components::layout::layout::Layout;
 use fallout_ui::components::layout::menu::{Menu, MenuItem, MenuMode};
 use fallout_ui::components::layout::nav::Nav;
 use fallout_ui::components::typography::header::Header;
 use gloo::console::console;
 use yew::prelude::*;
+use yew_heroicons::size_24::solid::EnvelopeIcon;
 use yew_router::hooks::{use_navigator, use_route};
 
 #[function_component]
@@ -38,20 +40,36 @@ pub fn Root() -> Html {
 
     html! {
         <Layout>
-            <Nav class="flex space-x-8">
-                <div class="cursor-pointer" onclick={move |_| navigator.push(&Route::Home)}>
-                    <Header class="text-2xl text-white">{"Šarūnas' Blog"}</Header>
-                    <Header class="text-base text-white">{"Rust Full Stack Cloud Engineering"}</Header>
+            <div class="min-h-screen flex flex-col items-center bg-gray-100 ">
+                <Nav class="w-full flex justify-center bg-gray-800">
+                    <div class="flex w-full max-w-6xl justify-between items-center px-6">
+                        <div class="cursor-pointer text-left justify-left" onclick={move |_| navigator.push(&Route::Home)}>
+                            <Header class="text-2xl text-white">{"Šarūnas' Blog"}</Header>
+                            <Header class="text-base text-white">{"Rust Full Stack Cloud Engineering"}</Header>
+                        </div>
+                        <Menu<Route> class="flex space-x-8 justify-end" mode={MenuMode::Horizontal} items={items.clone()} default_selected={current_index}/>
+                    </div>
+                </Nav>
+                <div class="flex-grow w-full max-w-4xl px-4 py-6">
+                    <div class={"bg-white p-6 rounded-lg shadow-md"}>
+                        <Content class="h-fit">
+                            <RouterSwitch/>
+                        </Content>
+                    </div>
                 </div>
-                <Menu<Route> mode={MenuMode::Horizontal} items={items.clone()} default_selected={current_index}/>
-            </Nav>
-            <div class="flex-1 pb-10 pt-6 px-12 space-y-6">
-                // { breadcrumbs.clone() }
-                <div class={"bg-white p-6 rounded-lg shadow-md"}>
-                <Content>
-                    <RouterSwitch/>
-                </Content>
-                </div>
+                <Footer class="flex w-full justify-center space-x-4">
+                    <a href="mailto:sarunas.gincas@gmail.com">
+                        <EnvelopeIcon class="h-8 w-8 hover:opacity-75"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/sarunas-gincas-a91676211/" target="_blank">
+                        <img src="/images/linkedin.svg"
+                            alt="linkedin" class="h-8 w-8 hover:opacity-75"/>
+                    </a>
+                    <a href="https://github.com/Saruniks" target="_blank">
+                        <img src="/images/github.png"
+                            alt="github" class="h-8 w-8 hover:opacity-75"/>
+                    </a>
+                </Footer>
             </div>
         </Layout>
     }
--- src/root_page.rs
+++ src/root_page.rs
@@ -1,12 +1,38 @@
-use fallout_ui::components::typography::header::Header;
+use fallout_ui::components::{
+    card::Card,
+    layout::grid::{Columns, Grid},
+    link::primary_link::PrimaryLink,
+    typography::{body_text::BodyText, header::Header, small_body_text::SmallBodyText},
+};
 use yew::prelude::*;
+
+use crate::routes::Route;
 
 #[function_component]
 pub fn RootPage() -> Html {
     html! {
         <>
-            <Header class="text-4xl">{"Šarūnas Ginčas"}</Header>
-            <Header class="mb-4 text-xl">{"Rust Software Engineer"}</Header>
+            <Header class="text-4xl mb-4">{"About me"}</Header>
+            <div class="space-y-4">
+            <BodyText>
+                { "I'm a Rust Software Engineer from Lithuania." }
+            </BodyText>
+            <BodyText>
+                { "I am using Rust in Frontend (Yew) and Backend development." }
+            </BodyText>
+            <BodyText>
+                { "For cloud infrastructure, I mostly use AWS CDK, and for application development and interacting with AWS services, I use AWS SDK." }
+            </BodyText>
+                <BodyText>
+                    { "View my " }
+                    <PrimaryLink<Route> to={Route::Blog}>{"Blog"}</PrimaryLink<Route>>
+                    { ", "}
+                    <PrimaryLink<Route> to={Route::Projects}>{"Projects"}</PrimaryLink<Route>>
+                    { ", or send me an email at " }
+                        <a class={"text-primary text-base hover:underline"} href="mailto:sarunas.gincas@gmail.com">{"sarunas.gincas@gmail.com"}</a>
+                    { " if you would like to learn more." }
+                </BodyText>
+            </div>
         </>
     }
 }
